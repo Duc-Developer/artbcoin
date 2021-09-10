9c5cecd9c8a13154300aefcb5fff924141f891e7
DucTT <mrtranduc1994@outlook.com>
2021-09-11 00:18:54 +0700

    fix nav mobi

--- src/layouts/Navbar/index.tsx
+++ src/layouts/Navbar/index.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import logo from 'assets/icons/logo.svg';
+import bgToggle from 'assets/images/mobile/bg-menu-toggle.png';
+import toggleIcon from 'assets/icons/menu.svg';
 import './style.scss';
+
+const toggleStyled = {
+    backgroundImage: `url(${bgToggle})`,
+    backgroundSize: 'cover',
+};
 
 const MainNavbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,11 +16,15 @@ const MainNavbar: React.FC = () => {
     const toggle = () => setIsOpen(!isOpen);
 
     return (
-        <Navbar id="main-navbar" color="light" light expand="md">
-            <NavbarBrand href="/">
-                <img src={logo} alt="" />
-            </NavbarBrand>
-            <NavbarToggler id="toggle-button" onClick={toggle} />
+        <Navbar id="main-navbar" style={!isOpen ? {} : toggleStyled} color="light" light expand="md">
+            <div className="header">
+                <NavbarBrand href="/">
+                    <img src={logo} alt="" />
+                </NavbarBrand>
+                <NavbarToggler id="toggle-button" onClick={toggle}>
+                    <img src={toggleIcon} alt="toggle" />
+                </NavbarToggler>
+            </div>
             <Collapse isOpen={isOpen} navbar>
                 <Nav className="mr-auto" navbar>
                     <NavItem>
